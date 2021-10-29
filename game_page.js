e2287ce3842bc581c61b6e27524eec00dd15477f
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2021-10-29 17:29:15 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,31 @@
+
+	player1_name = localStorage.getItem("player1_name");
+	player2_name = localStorage.getItem("player2_name");
+
+	player1_score = 0;
+	player2_score = 0;
+
+	document.getElementById("player1_name").innerHTML = "<b>"+player1_name+" :</b>" ;
+	document.getElementById("player2_name").innerHTML = "<b>"+player2_name +" :</b> " ;
+
+document.getElementById("player1_score").innerHTML ="<kbd>" +player1_score+"</kbd>" ;
+document.getElementById("player2_score").innerHTML = "<kbd>"+player2_score+"</kbd>" ;
+
+document.getElementById("player_question").innerHTML = "Question Turn - <kbd> " + player1_name +"</kbd>";
+document.getElementById("player_answer").innerHTML = "Answer Turn - <kbd>" + player2_name +"</kbd>" ;
+
+function send() {
+	number1 = document.getElementById("number1").value;
+	number2 = document.getElementById("number2").value;
+	actual_answer = parseInt(number1) * parseInt(number2);
+	console.log(actual_answer);
+
+
+    question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_number + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+	document.getElementById("number1").value = "";
+	document.getElementById("number2").value = "";
+}
